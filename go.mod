bb5d566ee91f4df1c8cae399d14c4fe39aabe21e
Sergio <SG Computers>
2025-10-03 17:03:34 +0300

    uuid v7 type

---
+++ go.mod
@@ -0,0 +1,5 @@
+module acaodefs
+
+go 1.25.1
+
+require github.com/moroz/uuidv7-go v0.0.0-20240305042206-a7e3dca2a87e
---
+++ types/uuid.go
@@ -0,0 +1,49 @@
+package types
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/moroz/uuidv7-go"
+)
+
+type UUID struct {
+	uuidv7.UUID
+}
+
+func NewUUID() UUID {
+	return UUID{uuidv7.Generate()}
+}
+func ParseUUID(s string) (UUID, error) {
+	parsed, err := uuidv7.Parse(s)
+	if err != nil {
+		return UUID{}, fmt.Errorf("invalid UUID format: %w", err)
+	}
+	return UUID{parsed}, nil
+}
+func ZeroUUID() UUID {
+	uuid, err := uuidv7.Parse("00000000-0000-0000-0000-000000000000")
+	if err == nil {
+		return UUID{uuid}
+	}
+	return NewUUID()
+}
+func (u UUID) String() string {
+	return u.UUID.String()
+}
+func (u *UUID) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("UUID must be a string")
+	}
+	parsed, err := uuidv7.Parse(str)
+	if err != nil {
+		return fmt.Errorf("invalid UUID format: %w", err)
+	}
+	u.UUID = parsed
+	return nil
+}
+
+func (u UUID) MarshalGQL(w io.Writer) {
+	fmt.Fprintf(w, `"%s"`, u.UUID.String())
+}
